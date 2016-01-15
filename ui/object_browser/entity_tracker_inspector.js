22d9b34e1cdc166ade67991e7717e9e9f221b4e0
Yang Shan <yang@radiant-entertainment.com>
2016-01-14 16:48:22 -0800

    add an entity tracker inspector that can be opened by the object browser

---
+++ ui/object_browser/entity_tracker_inspector.js
@@ -0,0 +1,55 @@
+
+App.StonehearthEntityTrackerInspectorView = App.View.extend({
+   templateName: 'entityTrackerInspector',
+   uriProperty: 'model',
+   closeOnEsc: true,
+   
+   init: function() {
+      var self = this;
+      radiant.call_obj('debugtools.entity_tracker', 'load_entities_command')
+         .done(function(response) {
+               self.set('uri', response.tracker);
+               self.set('num_entities', response.num_entities)
+            });
+      self._super();
+   },
+
+   didInsertElement: function() {
+      // look at entities
+      this.$('#body').on("click", ".navlink", function(event) {
+         var uri = $(this).attr('href');
+         var existingDebugView = App.debugView.getView(App.StonehearthObjectBrowserView);
+         App.debugView.addView(App.StonehearthObjectBrowserView, { uri: uri });
+      });
+   },
+
+   all_entities: function() {
+      var self = this;
+      var entities_dictionary = self.get('model.entities');
+      var entities_array = [];
+      var index = 0;
+      radiant.each(entities_dictionary, function(k, v) {
+         var uri = k;
+         if (uri == "") {
+            uri = "[no uri]"
+         }
+         entities_array[index] = {
+            count : v.count,
+            uri : uri,
+            data : v.data
+         };
+         index++;
+      });
+
+      entities_array.sort(function(a, b){
+         return b.count - a.count;
+      });
+      return entities_array;
+   }.property('model.entities'),
+
+   actions: {
+      close: function () {
+         this.destroy();
+      },
+   }
+});
