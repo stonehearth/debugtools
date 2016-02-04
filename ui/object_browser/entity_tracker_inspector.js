bdacd782348ebdc52f000a13c5c660d5df683f69
Yang Shan <yang@radiant-entertainment.com>
2016-02-03 18:48:26 -0800

    Add ability to sort the entity tracker inspector

--- ui/object_browser/entity_tracker_inspector.js
+++ ui/object_browser/entity_tracker_inspector.js
@@ -6,6 +6,8 @@ App.StonehearthEntityTrackerInspectorView = App.View.extend({
    
    init: function() {
       var self = this;
+      self._isAsc = false;
+      self._setSortFunction('count', self._isAsc);
       radiant.call_obj('debugtools.entity_tracker', 'load_entities_command')
          .done(function(response) {
                self.set('uri', response.tracker);
@@ -15,15 +17,44 @@ App.StonehearthEntityTrackerInspectorView = App.View.extend({
    },
 
    didInsertElement: function() {
+      var self = this;
       // look at entities
-      this.$('#body').on("click", ".navlink", function(event) {
+      self.$('#body').on("click", ".navlink", function(event) {
          var uri = $(this).attr('href');
          var existingDebugView = App.debugView.getView(App.StonehearthObjectBrowserView);
          App.debugView.addView(App.StonehearthObjectBrowserView, { uri: uri });
       });
+
+      self.$('#countHeader').click(function(event) {
+         self._isAsc = !self._isAsc;
+         self._setSortFunction('count', self._isAsc);
+         self._updateAllEntities();
+      });
+
+      self.$('#AliasHeader').click(function(event) {
+         self._isAsc = !self._isAsc;
+         self._setSortFunction('uri', self._isAsc);
+         self._updateAllEntities();
+      });
    },
 
-   all_entities: function() {
+   _setSortFunction: function(sort_field, isAsc) {
+      this._sort = function(array) {
+         array.sort(function(a, b){
+            var a_value = a[sort_field];
+            var b_value = b[sort_field];
+            if ((typeof a_value) == 'string') {
+               return isAsc ? a_value.localeCompare(b_value) : b_value.localeCompare(a_value);
+            }
+            return isAsc ? (a_value - b_value) : (b_value - a_value);
+         });
+      };
+      var suffix = isAsc ? 'asc' : 'desc';
+      this.set('sort', {});
+      this.set('sort.' + sort_field + '_' + suffix, true);
+   },
+
+   _updateAllEntities: function() {
       var self = this;
       var entities_dictionary = self.get('model.entities');
       var entities_array = [];
@@ -41,11 +72,9 @@ App.StonehearthEntityTrackerInspectorView = App.View.extend({
          index++;
       });
 
-      entities_array.sort(function(a, b){
-         return b.count - a.count;
-      });
-      return entities_array;
-   }.property('model.entities'),
+      self._sort(entities_array);
+      self.set('all_entities', entities_array);
+   }.observes('model.entities'),
 
    out_of_bounds_entities: function() {
       var self = this;
